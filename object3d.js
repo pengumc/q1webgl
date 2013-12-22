15ecf43dc1047f7785207727a3a98b6374cc217d
Pengu MC @ vito <michiel.van.der.coelen@gmail.com>
2013-12-22 23:08:26 +0100

    basic setup

--- object3d.js
+++ object3d.js
@@ -200,13 +200,13 @@ var base_cube_normals = new Float32Array([
     -1.0, 0.0, 0.0
 ]);
 
+
 function matrix_mult(a, b){
     var a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
         a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7],
         a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11],
         a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
     var out = new Array(16);
-    // Cache only the current line of the second matrix
     var b0 = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
     out[0] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
     out[1] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
