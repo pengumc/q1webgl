f4d080607f4d17e776d45f29e8297b337799e976
michiel van der Coelen <michiel.van.der.coelen@gmail.com>
2013-12-27 17:23:16 +0100

    purge useless code

--- main.js
+++ main.js
@@ -25,11 +25,6 @@ function start() {
     Q1 = new Q1Model();
     Q1.configure_default();
     
-    lines = new Array();
-    var l = new Line3D();
-    l.setup_line(Q1.servos[0], Q1.servos[6]);
-    lines.push(l);
-    
     window.setInterval(function(){
         Q1.draw();
     }, 100);
@@ -92,29 +87,6 @@ function build_program2(){
     return(shader_program);
 }
 
-function add_cube(){
-    var cube = new Object3D();
-    cube.set_position(-2 + Math.random()*4,-2 + Math.random()*4, -Math.random()*20);
-    cube.load_cube();
-    cube.color[0] = Math.random();
-    cube.color[1] = Math.random();
-    cube.color[2] = Math.random();
-    cube.set_R(Math.random()*Math.PI*2, Math.random()*Math.PI*2, Math.random()*Math.PI*2);
-    cubes.push(cube);
-}
-
-function update(){
-    var nt = (new Date()).getTime();
-    var dt = (nt - t)*0.001;
-    t = nt;
-    phi += 0.3 * dt;
-    cubes[0].set_R(phi, 0, 0);
-    cubes[1].set_R(phi, 0, 0);
-    cubes[2].set_R(phi, 0, 0);
-    draw_objects(cubes);
-}
-
-
 function create_fragment_shader1(){
 	var src = document.getElementById("fragment_shader_src").innerHTML;
 	var fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -161,54 +133,9 @@ function generate_perspective_matrix(fov, near, far, w, h){
 }
 
 function set_R(theta, psi, phi){
-    //angles are along x y and z axis, in that order
-    // var ctheta = Math.cos(theta); //x
-    // var stheta = Math.sin(theta); //x
-    // var cphi = Math.cos(phi); //y
-    // var sphi = Math.sin(phi); //y
-    // var cpsi = Math.cos(psi); //z
-    // var spsi = Math.sin(psi); //z
-	// R_matrix[0] = cphi * cpsi;	
-	// R_matrix[1] = -cpsi * sphi;	
-	// R_matrix[2] = -spsi;	
-	// R_matrix[3] = 0;	
-
-	// R_matrix[4] = ctheta * sphi - cphi * stheta * spsi;	
-	// R_matrix[5] = ctheta * cphi + stheta *sphi * spsi;	
-	// R_matrix[6] = -cpsi * stheta;	
-	// R_matrix[7] = 0;	
-
-	// R_matrix[8] = stheta * sphi + ctheta * cphi * spsi;	
-	// R_matrix[9] = cphi * stheta - ctheta * sphi * spsi;	
-	// R_matrix[10] = ctheta * cpsi;	
-	// R_matrix[11] = 0;	
-
-	// R_matrix[12] = 0;	
-	// R_matrix[13] = 0;	
-	// R_matrix[14] = 0;	
-	// R_matrix[15] = 1;	
     gl.shader_program1.R_matrix.set(gen_R(theta, psi, phi));
 }
 
-function draw(){
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	//bind buffer, again?
-	//	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-	//setup vertex attrib pointer
-	gl.vertexAttribPointer(vertex_position_attribue, 3, gl.FLOAT, false, 0, 0);
-
-	
-	//gl.uniformMatrix4fv p_uniform, perspective matrix
-	gl.uniformMatrix4fv(p_uniform, false, p_matrix);
-	//gl.uniformMatrix4fv mv_uniform, general transform
-	gl.uniformMatrix4fv(mv_uniform, false, mv_matrix); 
-	
-
-	
-	//draw 4 items
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 24);
-}
 function scroll(e){
     if(e.deltaY<0){
         gl.shader_program1.mvc_matrix[14] += 1;
